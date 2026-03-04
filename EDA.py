83b1e646f272f6110443bc353b4ef0226c5e8986
Justin <test@test.com>
2026-03-04 11:04:13 +0000

    Updated files

--- EDA.py
+++ EDA.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
 #pull the csv files into pandas
-books = pd.read_csv(r"data\03_Library Systembook.csv")
-customers = pd.read_csv(r"data\03_Library SystemCustomers.csv")
+books = pd.read_csv("data/03_Library Systembook.csv")
+
 
 #remove any lines that have NaN value in the book title
 books = books.dropna(subset=["Books"])
@@ -18,7 +18,7 @@ def fix_invalid_date(x):
         return "31/05/2023"
     return x
 books["Book checkout"] = books["Book checkout"].apply(fix_invalid_date)
-books["Book checkout"] = books["Book checkout"].str.replace('"', '', regex=False)
+books["Book checkout"] = books["Book checkout"].str.replace('"', '', regex=True)
 
 #convert date strings to datetime format
 date_cols = ["Book checkout", "Book Returned"]
@@ -27,4 +27,12 @@ books[date_cols] = books[date_cols].apply(pd.to_datetime, dayfirst=True, errors=
 #add column to calculate days on loan
 books["Days on Loan"] = (books["Book Returned"] - books["Book checkout"]).dt.days
 
-print(books.head(n=20))
+#load customers file 
+customers = pd.read_csv("data/03_Library SystemCustomers.csv")
+
+#remove NaN value entries in the Customers data
+customers_cleaned = customers.dropna()
+
+#create cleaned csv files
+books.to_csv("books_cleaned.csv")
+customers_cleaned.to_csv("customers_cleaned.csv")
